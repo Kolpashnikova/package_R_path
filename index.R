e5c138eed6cd3abfeeb7e663a7cf72e76598835b
Kamila Kolpashnikova <kamilakolpashnikova@gmail.com>
2023-09-13 18:26:06 -0400

    licence update

--- index.R
+++ index.R
@@ -1,5 +1,4 @@
 library(path)
-library(rjson)
 library(jsonlite)
 
 s <- fromJSON('ex_data.txt')
@@ -8,10 +7,8 @@ w <- fromJSON('weights.txt')
 act <- fromJSON('{
   "Sleep": 1, "Personal Care": 2,
   "Housework": 3, "Child Care": 4, "Adult Care": 5, "Work": 6,
-  "Shopping": 7, "TV Watching": 8, "Eating": 9, "Leisure": 10, "Travel": 11
-  }')
+  "Shopping": 7, "TV Watching": 8, "Eating": 9, "Leisure": 10, "Travel": 11,
+                "Other": 12, "Random": 13}')
 
 
-path(df = toJSON(s), act = act, w=w, layout = "circularCenter", focusActivity = "Sleep",
-     colors = c("#FF5733", "#33FF57", "#337AFF", "#FF33D4", "#FFFF33", "#4A90E2",
-                "#FFA500", "#8A2BE2", "#008000", "#800000", "#A52A2A"))
+path(df = toJSON(s), act = act, w=w, layout = "circularCenter", focusActivity = "Leisure")
